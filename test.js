8377f1e8a0eb08e6a49147540b3021684551427c
Icdan <li215@live.nl>
2017-12-18 11:43:08 +0100

    Starten met Bootstrap

--- test.js
+++
@@ -1,4 +0,0 @@
-// this cannot be empty
-// i will not allow it
-// YOU SHALL NOT PASS
-// RIDE, RIDE FOR RUIN AN THE WORLD'S ENDING
--- test2.js
+++
@@ -1,5 +0,0 @@
-// this cannot be empty
-// i will not allow it
-// YOU SHALL NOT PASS
-// RIDE, RIDE FOR RUIN AN THE WORLD'S ENDING
-// Persians, COME AND GET THEM
