66a3f816a46ca7b9d22628b1f56e59bc9f19d944
Icdan <li215@live.nl>
2018-01-14 22:00:40 +0100

    Changing some images

--- test.js
+++ test.js
@@ -6,6 +6,8 @@ function toggle() {
 
 var n = 0;
 var m = 0;
+var l = 0;
+var z = 0;
 
 function toggleMan() {
     if (n == 0) {
@@ -26,6 +28,28 @@ function toggleOrk() {
     else {
         document.getElementById('intro-img2').src = "testimg/ork.png";
         m = 0;
+    }
+}
+
+function toggleTau() {
+    if (l == 0) {
+        document.getElementById('intro-img2').src = "testimg/Tau02.png";
+        l = 1;
+    }
+    else {
+        document.getElementById('intro-img2').src = "testimg/Tau.png";
+        l = 0;
+    }
+}
+
+function toggleChaos() {
+    if (z == 0) {
+        document.getElementById('intro-img').src = "testimg/ChaosStar02.png";
+        z = 1;
+    }
+    else {
+        document.getElementById('intro-img').src = "testimg/ChaosStar.png";
+        z = 0;
     }
 }
 
