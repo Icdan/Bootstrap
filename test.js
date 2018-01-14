bea2c973493569e37ed66a31bc1e96a7330d96ca
Icdan <li215@live.nl>
2018-01-14 20:24:47 +0100

    Nightmode

--- test.js
+++ test.js
@@ -3,3 +3,32 @@ function toggle() {
     a.x = 'smthn' == a.x ? 'something' : 'smthn'; // short if
     a.href = a.x + '.css';
 }
+
+var n = 0;
+var m = 0;
+
+function toggleMan() {
+    if (n == 0) {
+        document.getElementById('intro-img').src = "testimg/Aquila02.png";
+        n = 1;
+    }
+    else {
+        document.getElementById('intro-img').src = "testimg/Aquila.png";
+        n = 0;
+    }
+}
+
+function toggleOrk() {
+    if (m == 0) {
+        document.getElementById('intro-img2').src = "testimg/ork02.png";
+        m = 1;
+    }
+    else {
+        document.getElementById('intro-img2').src = "testimg/ork.png";
+        m = 0;
+    }
+}
+
+// localStorage.setItem('darkWasPressed', 'true');
+//
+// localStorage.getItem('darkWasPressed');
