d362eb3cb8ca7f62f27dd1679257c7179184aafb
Icdan <li215@live.nl>
2018-01-14 16:26:20 +0100

    Nightmode

--- test.js
+++ test.js
@@ -1,17 +1,5 @@
-function toggleNightMode() {
-    var color = document.getElementById('wrapper').style.color;
-
-    var backgroundColor = document.getElementById('wrapper').style.backgroundColor;
-
-
-    if (color == "black" && backgroundColor == "white") {
-        document.getElementById('mydiv').style.color="white";
-        document.getElementById('mydiv').style.backgroundColor="black";
-        document.getElementById('mydiv').style.fontFamily="Arial";
-
-    } else {
-        document.getElementById('mydiv').style.color="black";
-        document.getElementById('mydiv').style.backgroundColor="white";
-        document.getElementById('mydiv').style.fontFamily="Times New Roman";
-    }
+function toggle() {
+    var a = document.getElementById("style1");
+    a.x = 'smthn' == a.x ? 'something' : 'smthn'; // short if
+    a.href = a.x + '.css';
 }
