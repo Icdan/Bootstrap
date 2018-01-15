62e330085e7e6fc5f8cd2018adbfa1067aa60553
Icdan <li215@live.nl>
2018-01-15 10:01:04 +0100

    initial cookies

--- test.js
+++ test.js
@@ -2,6 +2,41 @@ function toggle() {
     var a = document.getElementById("style1");
     a.x = 'smthn' == a.x ? 'something' : 'smthn'; // short if
     a.href = a.x + '.css';
+    setCookie('css', a.x, 2);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setcss() {
+    var a = document.getElementById("style1");
+    file = getCookie('css') === null ? 'something' : getCookie('css');
+    a.href = getCookie('css') + '.css';
+}
+
+function nightModeActive() {
+
+    sessionStorage.setItem("nightModeIsActive")
 }
 
 var n = 0;
@@ -52,6 +87,8 @@ function toggleChaos() {
         z = 0;
     }
 }
+
+
 
 // localStorage.setItem('darkWasPressed', 'true');
 //
